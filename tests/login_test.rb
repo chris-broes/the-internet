12b4a86827a6f6d40ac1149015d17a830a516736
chris-broes <cb@saucelabs.com>
2016-09-13 14:49:34 -0700

    Running with Tunnel Identifier

--- tests/login_test.rb
+++ tests/login_test.rb
@@ -6,7 +6,7 @@ caps1 = Selenium::WebDriver::Remote::Capabilities.internet_explorer
 caps1['platform'] = 'Windows 7'
 caps1['version'] = 'latest'
 caps1["name"] = "Selenium with Sauce on IE 9"
-#caps1["tunnelIdentifier"] = "35.1"
+caps1["tunnelIdentifier"] = ENV['TRAVIS_BUILD_NUMBER']
 
 
 caps2 = Selenium::WebDriver::Remote::Capabilities.new
@@ -14,21 +14,21 @@ caps2["browserName"] = "Chrome"
 caps2["version"] = "latest"
 caps2["platform"] = "Linux"
 caps2["name"] = "Selenium with Sauce on Chrome"
-#caps2["tunnelIdentifier"] = "35.1"
+caps2["tunnelIdentifier"] = ENV['TRAVIS_BUILD_NUMBER']
 
 caps3 = Selenium::WebDriver::Remote::Capabilities.new
 caps3["browserName"] = "Firefox"
 caps3["version"] = "45.0"
 caps3["platform"] = "OS X 10.10"
 caps3["name"] = "Selenium with Sauce on Firefox"
-#caps3["tunnelIdentifier"] = "35.1"
+caps3["tunnelIdentifier"] = ENV['TRAVIS_BUILD_NUMBER']
 
 caps4 = Selenium::WebDriver::Remote::Capabilities.new
 caps4["browserName"] = "Internet Explorer"
 caps4["platform"] = 'Windows 8.1'
 caps4["version"] = '11.0'
 caps4["name"] = "Selenium with Sauce on IE 11"
-#caps4["tunnelIdentifier"] = "35.1"
+caps4["tunnelIdentifier"] = ENV['TRAVIS_BUILD_NUMBER']
 
 caps = [caps1, caps2, caps3, caps4]
 
